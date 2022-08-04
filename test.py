1b81198ecc20776aada2d319d975dd3306bedaad
abhirajp075@gmail.com <abhirajp075@gmail.com>
2022-08-04 21:49:35 +0530

    test files

---
+++ test.py
@@ -0,0 +1,29 @@
+
+from turtle import distance
+
+
+class place:
+    def __init__(self,*args):
+        if (len(args)==0):
+            self.name=None
+            self.connection=[]
+        
+        else:
+            self.name=args[0]
+            self.connection=[]
+class tree:
+    def __init__(self,name) :
+        a=place(name)
+        self.start=a
+
+    def add_connection(self,count):
+        inserter=[]
+        for i in range(count):
+            placeName=str(input("Enter the place name : "))
+            obj=place(placeName)
+            inserter.append(obj)
+            distance=int(input("Enter the distance from the root : "))
+            inserter.append(distance)
+            self.connection.append(inserter)
+            
+    
