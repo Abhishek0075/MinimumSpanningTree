05d26ded3dce7c6825c7516d9d8668d04f074b04
abhirajp075@gmail.com <abhirajp075@gmail.com>
2022-10-27 11:19:19 +0530

    new changes

--- inputMaker/createName.py
+++ inputMaker/createName.py
@@ -1,9 +1,10 @@
 # Change the number of nodes in the loop range and get the output automatically in the verthe.txt file
 import sys
+numberOfNames=50
 f=open(r"C:\Users\abhir\OneDrive\Documents\GitHub\project_ds\csvFileAndNames\names.txt",'w')
 sys.stdout=f
-for i in range(20):
-    if(i==19):
+for i in range(numberOfNames):
+    if(i==(numberOfNames-1)):
         print(i,end="")
         break
     print(i)
